66a285fd5cc9bd81aead863a0180c188d116daa3
skmr_manipulator <62818403+sukesh9055@users.noreply.github.com>
2021-04-28 20:06:31 +0530

    Code file created.

---
+++ super-hero/code.py
@@ -0,0 +1,20 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#Reading of the file
+df=pd.read_csv(path)
+
+df["Gender"].replace("-","Agender", inplace=True)
+df.corr()
+total_high=df["Total"].quantile(q=0.99)
+super_best=df[df["Total"]>total_high]
+super_best_names=list(super_best["Name"])
+print(super_best_names)
+# Code starts here
+
+
+
